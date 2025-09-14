c6987b373a19ced9ea0a191d19e37308f24f165c
Devran Turson <dvturson@gmail.com>
2025-09-14 17:55:44 -0400

    Removed inputbox, decreased repetition of texts

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const quotes = [
 ];
 
 let currentQuote; 
+let typedValue = "";
 let startTime;
 let endTime;
 
@@ -19,7 +20,7 @@ function getRandomQuote () {
     return quotes[randomIndex];
 }
 
-// 
+// replaced string quote with char span quote
 function displayQuote(quote) {
     quoteDisplay.innerHTML = "";
     const characters = quote.split("");
@@ -30,13 +31,66 @@ function displayQuote(quote) {
 
         quoteDisplay.appendChild(span);
     });
+}
+
+function updateQuoteDisplay() {
+    const quoteSpans = quoteDisplay.querySelectorAll("span");
+    console.log(quoteSpans);
+
+    if (!startTime) {
+        startTime = new Date();
+    }
+
+    let correctChars = 0;
+
+    quoteSpans.forEach((span, index) => {
+        const typedChar = typedValue[index];
+
+        if (!typedChar) {
+            span.classList.remove("correct", "wrong", "correctSpace", "wrongSpace");
+        } else if (typedChar == span.textContent) {
+            if (typedChar === " " ) {
+                span.classList.add("correctSpace");
+                span.classList.remove("wrongSpace");
+            } else {
+                span.classList.add("correct");
+                span.classList.remove("wrong");
+                correctChars ++;
+            }
+        } else {
+            if (span.textContent == " ") {
+                span.classList.add("wrongSpace");
+                span.classList.remove("correctSpace");
+            } else {
+                span.classList.add("wrong");
+                span.classList.remove("correct");
+            }
+        }
+    })
     
+    if (currentQuote === typedValue || currentQuote.length == typedValue.length) {
+        console.log("finished");
+        endTime = new Date();
+        const timeTaken  = (endTime - startTime) / 1000 / 60;
+        const wordCount = currentQuote.split(" ").length;
+        const charCount  = currentQuote.length;
+        const wpm = Math.round(wordCount / timeTaken);
+        const cps = Math.round(charCount / timeTaken / 60);
+
+        const accuracy = Math.round((correctChars / charCount) * 100);
+
+        document.getElementById("wordSpeed").textContent = wpm;
+        document.getElementById("charSpeed").textContent = cps;
+        document.getElementById("accuracy").textContent = accuracy;
+
+
+    }
+    // console.log(currentQuote, typedValue)
 }
 
 // START BUTTON
 startBtn.addEventListener("click", () => {
-    quoteInput.value = "";
-    quoteInput.focus();
+    typedValue = "";
     const randomQuote = getRandomQuote();
     currentQuote = randomQuote; // used to check finish
     displayQuote(randomQuote);
@@ -44,54 +98,25 @@ startBtn.addEventListener("click", () => {
     // reset results
     document.getElementById("wordSpeed").textContent = "0";
     document.getElementById("charSpeed").textContent = "0";
-
     document.getElementById("accuracy").textContent = "0";
     startTime = null;
 })
 
-// INPUT BOX
-quoteInput.addEventListener("input", () => {
-    const typedValue = quoteInput.value;
-    const currentSpans = quoteDisplay.querySelectorAll("span");
-    console.log(currentSpans);
-    
-    if (!startTime) {
-        startTime = new Date();
+// TYPED KEY
+document.addEventListener("keydown", (e) => {
+    if (!currentQuote) return;
+
+    const key = e.key;
+
+    if (key === " " || key === "Spacebar") {
+        e.preventDefault();
     }
 
-    let correctChars = 0;
-
-    currentSpans.forEach((span, index) => {
-        const typedChar = typedValue[index];
-
-        if (typedChar == null) {
-            span.classList.remove("correct", "wrong");
-        } else if (typedChar == span.textContent) {
-            span.classList.add("correct");
-            span.classList.remove("wrong");
-            correctChars ++;
-        } else {
-            span.classList.add("wrong");
-            span.classList.remove("correct");
-        }
-    })
-    
-    if (currentQuote === typedValue || currentQuote.length == typedValue.length) {
-        console.log("finished");
-        endTime = new Date;
-        const timeTaken  = (endTime - startTime) / 1000 / 60;
-        const wordCount = currentQuote.split(" ").length;
-        const charCount  = currentQuote.length;
-        const wpm = Math.round(wordCount / timeTaken);
-        const cpm = Math.round(charCount / timeTaken);
-
-        const accuracy = Math.round((correctChars / charCount) * 100);
-
-        document.getElementById("wordSpeed").textContent = wpm;
-        document.getElementById("charSpeed").textContent = cpm;
-        document.getElementById("accuracy").textContent = accuracy;
-
-
+    if (key === "Backspace") {
+        typedValue = typedValue.slice(0, -1);
+    } else if (key.length == 1) {
+        typedValue += key;
     }
-    console.log(currentQuote, typedValue)
+
+    updateQuoteDisplay();
 })
